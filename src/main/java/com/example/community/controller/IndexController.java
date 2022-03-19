9b088e7d13448e3dd9d1f42c91356b1a28e6c32a
xuxinhang233 <914219004@qq.com>
2022-03-19 14:20:03 +0800

    初次使用UI和模板

--- src/main/java/com/example/community/controller/IndexController.java
+++ src/main/java/com/example/community/controller/IndexController.java
@@ -7,12 +7,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
-    @GetMapping("/greeting")
+    @GetMapping("")
     // 接收一个参数，名为 "name"
     public String greeting(@RequestParam(name = "name", required = false, defaultValue = "world")
                            String name, Model model) {
         model.addAttribute("name", name);
-        return "greeting";
+        return "index";
     }
 
 }
